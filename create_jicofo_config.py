c2207066c4a673322972b9531993175ec2f326c1
hbellur <hbellur@gmail.com>
2015-07-01 15:01:03 -0400

    Updated  jicofo config script
    1. script now registers user xrtc_sp00f_f0cus as a  prosody user

--- create_jicofo_config.py
+++ create_jicofo_config.py
@@ -10,24 +10,32 @@
 
 """
 
-import sys, os
+import sys, os, re
 
 def create_jicofo_config(prosody_section, jicofo_section):
 	
 	print "config file path is %s" %jicofo_section['config_file_path']
 	jicofo_config_file = open(os.path.join(jicofo_section['config_file_path'], "config"),'w')
+	domain =  prosody_section['domain']
+	
 	# create the lines 
 	line1 = "# Jitsi Conference Focus settings\n"
 	line2 = "JICOFO_HOST=localhost\n"
 	line3 = "JICOFO_HOSTNAME=" + prosody_section['domain'] + "\n"
 	line4 = "JICOFO_SECRET=Xtjn@I1#\n"
 	line5 = "JICOFO_PORT=5347\n"
-	line6 = "JICOFO_AUTH_DOMAIN=auth." + prosody_section['domain'] + "\n"
+	line6 = "JICOFO_AUTH_DOMAIN=auth." + domain + "\n"
 	subdomain = prosody_section['focus_fqdn'].split(".")[0]
-	if (subdomain != 'focus'):
-		line7 = "JICOFO_SUBDOMAIN=" + subdomain + "\n"
+	# register an account with "subdomain" as the user and "JICOFO_SECRET" as the password
+	register_string = "prosodyctl register " + subdomain + " auth." + domain + " Xtjn@I1#"
+	os.system(register_string)
+	
+	#determine if domain is IPv4 or IPv6 address.
+	#check if domain contains numbers and '.' only
+	if re.match("^[\d.]*$", domain):
+		line7 = ""
 	else:
-		line7=""
+		line7 = "JICOFO_SUBDOMAIN=" + subdomain + "\n"
 	line8 = "JICOFO_AUTH_USER=xrtc_sp00f_f0cus\nJICOFO_AUTH_PASSWORD=xrtc_sp00f_f0cus\nJICOFO_OPTS=\"\"\n"
 	
 	# write all the lines to the file
